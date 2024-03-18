8cc4462d16727a68c1b6e0918119cf3bcad88713
Silver-Eun <h22sang@naver.com>
2024-03-18 23:50:51 +0900

    Ch1 - 13 ~ 16
    Keyboard Shortcut

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -2,6 +2,16 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		// 단축키
+		// ctrl + shift + l : 단축키 전체 목록
+		// ctrl + +, - : 폰트 크기 증가 / 감소
+		// ctrl + d : 한줄 삭제
+		// ctrl + alt + down : 행단위 복사
+		// alt + shift + a : 멀티컬러 편집
+		// alt + up, down : 행단위 이동
+		// ctrl + i : 자동 들여쓰기
+		// ctrl + / : 주석
+		// ctrl + space : 자동 완성
 		System.out.println("Hello, World");
 	}
 
