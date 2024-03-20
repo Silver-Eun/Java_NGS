5868f0853b89a7507ecf4c71d23c7183929eab8a
Silver-Eun <h22sang@naver.com>
2024-03-20 19:34:14 +0900

    Ch2 - 1 ~ 2
    Printout

---
+++ _ch02/src/Ex2_1.java
@@ -0,0 +1,15 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// print : 줄바꿈 안하고 출력
+		// println : 줄바꿈 출력
+		System.out.print("3+5");
+		System.out.println(5 + 3);
+		System.out.println(5 - 3);
+		System.out.println(5 * 3);
+		System.out.println(5 / 3);
+		System.out.println(5 / 3.0);
+	}
+
+}
