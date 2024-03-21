2833c75fec39eb3f986da1b6ce1ed6ad8c521c57
Silver-Eun <h22sang@naver.com>
2024-03-21 17:38:11 +0900

    Ch2 - 7 ~ 8
    String, String Literals, Combine char

--- _ch02/src/VarEx3.java
+++ _ch02/src/VarEx3.java
@@ -10,19 +10,18 @@ public class VarEx3 {
 		System.out.println(power);
 
 		byte b = 127;
+		System.out.println(b);
 
 		// 8진수
 		int oct = 010;
 		// 16진수
 		int hex = 0x10;
-
 		System.out.println(oct);
 		System.out.println(hex);
 
 		long l = 10_0000_0000;
-
 		System.out.println(l);
-		
+
 		float f = 3.14f;
 		double d = 3.14f;
 		System.out.println(f);
@@ -32,6 +31,18 @@ public class VarEx3 {
 		System.out.println(.10);
 		System.out.println(10f);
 		System.out.println(1e3);
+
+		char ch = 'A';
+		int i = 'A';
+		System.out.println(ch);
+		System.out.println(i);
+
+		String str = "";
+		String str2 = "ABCD";
+		String str3 = "123";
+		String str4 = str2 + str3;
+		System.out.println(str);
+		System.out.println(str4);
 	}
 
 }
