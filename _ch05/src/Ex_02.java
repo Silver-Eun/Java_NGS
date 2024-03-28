a541e954b78a82296cfa86d9324a17dacde0a3c5
Silver-Eun <h22sang@naver.com>
2024-03-28 18:32:57 +0900

    Ch5 - 4 ~ 5
    Length of Array, Reset of Array

---
+++ _ch05/src/Ex_02.java
@@ -0,0 +1,13 @@
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10]; // 길이가 5인 int 배열 arr 생성
+		System.out.println("arr.length = " + arr.length);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("arr[" + i + "] =" + arr[i]);
+		}
+	}
+
+}
