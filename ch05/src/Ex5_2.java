c4c483b58063ac8d39c5a247fd8903bd66ceb435
Silver-Eun <h22sang@naver.com>
2024-03-28 21:10:33 +0900

    Ch5 - 8 ~ 11
    Using Array(1) ~ (4)

--- ch05/src/Ex5_2.java
+++ ch05/src/Ex5_2.java
@@ -1,16 +1,16 @@
 class Ex5_2 {
 	public static void main(String[] args) {
-		int   sum = 0;      // ÃÑÁ¡À» ÀúÀåÇÏ±â À§ÇÑ º¯¼ö
-		float average = 0f; // Æò±ÕÀ» ÀúÀåÇÏ±â À§ÇÑ º¯¼ö
+		int sum = 0;
+		float average = 0f;
 
-		int[] score = {100, 88, 100, 100, 90};
+		int[] score = { 100, 88, 100, 100, 90 };
 
-		for (int i=0; i < score.length ; i++ ) {
+		for (int i = 0; i < score.length; i++) {
 			sum += score[i];
 		}
-		average = sum / (float)score.length ; // °è»ê°á°ú¸¦ float·Î ¾ò±â À§ÇØ¼­ Çüº¯È¯
+		average = sum / (float) score.length;
 
-		System.out.println("ÃÑÁ¡ : " + sum);
-		System.out.println("Æò±Õ : " + average);
+		System.out.println("ì´ì  : " + sum);
+		System.out.println("í‰ê·  : " + average);
 	}
 }
